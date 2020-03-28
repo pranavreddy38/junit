6fe14af3716aca1ddf349f4e4e81f82e7a48fc6f
pranavreddy38 <pranavreddy38@gmail.com>
2020-03-28 19:30:11 +0530

    commited

---
+++ junit/src/main/java/SearchFirst2Chars.java
@@ -0,0 +1,27 @@
+
+
+public class SearchFirst2Chars {
+	public String removeA(String string) {
+		int c=0;
+		String result="";
+		if(string.length()>=2) {
+		    for(int i=0;i<2;i++) {
+		        if(string.charAt(i)=='A') {
+			         c=c+1;
+		        }
+		    }
+		result=string.substring(c);		
+	   }
+		else if(string.length()==1) {
+			if(string.charAt(0)=='A') {
+		         c=c+1;
+	        }
+			result=string.substring(c);
+		}
+		else {
+			result=string;
+		}
+	return result;	
+	}
+
+}
